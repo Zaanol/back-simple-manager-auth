18cb305d100c0d85e53a5d314a54c54d95b0aa92
Willian Zanol <willianzanoll@hotmail.com>
2024-03-05 21:12:39 -0300

    Test deploy Render

--- db/db.js
+++ db/db.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 async function connect() {
     mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log('Connected to MongoDB');
+
+        const testSchema = new mongoose.Schema({
+            name: String
+        });
+    
+        const TestEntity = mongoose.model('Test', testSchema);
     }).catch((error) => 
         console.error(error)
     );
